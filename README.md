48c45da1bd9dbfaf32c0ba345a75e3fa86187413
J Mark Hord <presuppositionalist@yahoo.com>
2017-02-20 09:55:29 -0500

    Refactored project with junit testing additions for coverage.

--- README.md
+++ README.md
@@ -1,14 +1,33 @@
 # Foo
 Anthem Test project
 
+# Version 1 notes
 The first version is the original code with the bugs and all.
 
 The second version is just some bare essential changes to achieve the desired
-outputs along with a simple junit test to automate checking that output.  
+outputs along with a simple junit test to automate checking that output.
 One of the main problems with the original was that it used float and double
 types for money.  I debated using one of the newer money-oriented utility
 packages but decided the most direct conversion would be to just go with 
 the BigDecimal approach to keep the changes to a minimum.  Made a few other
 minor changes just to make it a little more readable toward the last version
 still to be done.
+
+# Version 2 notes
+The second version is a refactoring to make it easier to maintain.  Primarily
+that meant changing variable names to more meaningful names for the context. 
+Also wanted to break up the classes to more easily divide the work between a group of team members as well as separating the presentation and tax calculation models from the data model as much as possible.
+
+Created a new class TaxCalculator to encapsulate the tax calculation and allow different instantiations to calculate domestic or import taxes. 
+
+Created a new OrderMoney class that wraps a BigDecimal. I chose to intentionally not extend BigDecimal so as not to introduce assumptions that they two are effectively interchangeable. This allows different rounding rules and limits the kinds of operations you can perform on OrderMoney to only what we expose to clients. 
+
+OrderMoney and TaxCalculator are set to USD Currency by default. If we wanted to extend the classes we would want to alter them to different names for each currency perhaps. But, in the context of this exercise, it seemed a stretch to do so and live with the assumption that we did not need to worry about converting different monetary values. A better approach for later versions of java would be to utilize the Money class developed more recently. 
+
+In addition, I wanted to seperate the data from the application by moving the hard-coded Order information to a resource XML file and encapsulating the unpacking to a seperate class that could, in theory, be altered to use a database or some other file format to contain the Order attributes. Perhaps it would make sense to make the Order class serializable but that seemed a debate for the design team to determine the approach to take.
+
+
+
+
+
 
--- test/com/tek/interview/question/testFoo.java
+++ test/com/tek/interview/question/testFoo.java
@@ -10,6 +10,8 @@ import org.junit.Before;
 import org.junit.Test;
 import java.io.PrintStream;
 import java.io.ByteArrayOutputStream;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * @author stratiotes
@@ -39,6 +41,119 @@ public class testFoo {
 	    System.setOut(null);
 	    System.setErr(null);
 	}
+	
+	/**
+	 * Test the Item class
+	 */
+	@Test
+	public void testItem() {
+		// domestic item
+		Item item = new Item("sample", 1.00);
+		assertEquals(item.getDescription(), "sample");
+		assertEquals(item.getPrice().toString(), "1");
+		
+		TaxCalculator tcalc = item.getTaxCalculator();
+		OrderMoney tax = tcalc.computeTax(item.getPrice());
+		
+		assertEquals(tax.toString(), "0.1");
+		
+		// import item
+		item = new Item("Imported sample", 1.00);
+		tcalc = item.getTaxCalculator();
+		tax = tcalc.computeTax(item.getPrice());
+		
+		assertEquals(tax.toString(), "0.15");
+		
+		// Special case that would fail with the initial 
+		// version of code that did not ignore desc string case in
+		// determining tax
+		
+		item = new Item("Caps IMPORTED test", 1.00);
+		tcalc = item.getTaxCalculator();
+		tax = tcalc.computeTax(item.getPrice());
+		
+		assertEquals(tax.toString(), "0.15");
+	}
+	
+	/**
+	 * Test the Order class
+	 */
+	@Test
+	public void testOrder() {
+		Order order = new Order("Testing Order");
+		
+		assertEquals(order.getName(), "Testing Order");
+		
+		assertEquals(order.size(), 0);
+		
+		Item item = new Item("Nine", 9.99);
+		OrderLine orderline = null;
+		try {
+			orderline = new OrderLine(item, 2);
+			
+		} catch (Exception e) {
+			fail("An exception was thrown in OrderLine creation");
+			e.printStackTrace();
+		}
+		
+		order.add(orderline);
+		assertEquals(order.size(), 1);
+		
+		assertNotNull(order.get(0));
+		
+		order.clear();
+		assertEquals(order.size(), 0);
+	}
+	
+	/**
+	 * Test of the XML OrderUnpacker
+	 */
+	@Test
+	public void testOrderUnpacker() {
+		Map<String, Order> OrdersMap = new TreeMap<String, Order>();
+		
+		OrderUnpacker orderUnpacker = new OrderUnpacker();
+		try {
+			orderUnpacker.unpackXML("resources/testingSet2.xml", OrdersMap);
+		} catch (Exception e) {
+			fail("The unpacker threw an exception parsing the test set file.");
+			e.printStackTrace();
+		}
+		
+		// check the second (of two) orderline of the second order
+		assertTrue(OrdersMap.size() == 2);
+		Order order = OrdersMap.get("Two Imports");
+		
+		assertNotNull(order);
+		
+		assertTrue(order.size() == 2);
+		
+		OrderLine oline = order.get(1);
+		assertNotNull(oline);
+		assertNotNull(oline.getItem());
+		assertEquals(oline.getQuantity(), 2);
+		
+		Item item = oline.getItem();
+		
+		assertEquals(item.getDescription(), "Item imported of Two");
+		
+		// check the second (of three) orderline of the first order
+		order = OrdersMap.get("One");
+		assertNotNull(order);
+		
+		assertTrue(order.size() == 3);
+		
+		oline = order.get(1);
+		assertNotNull(oline);
+		assertNotNull(oline.getItem());
+		assertEquals(oline.getQuantity(), 2);
+		
+		item = oline.getItem();
+		OrderMoney price = item.getPrice();
+		assertNotNull(price);
+		
+		assertEquals(price.toString(), "2.22");
+	}
 
 	/**
 	 * Simple test of the Foo main method
@@ -51,12 +166,12 @@ public class testFoo {
 		expected += "*******Order 2*******\n1 imported box of chocolate: 11.5\n1 imported bottle of perfume: 54.62\nSales Tax: 8.62\nTotal: 57.5\n";
 		expected += "*******Order 3*******\n1 Imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n1 packet of headache pills: 10.73\n1 box of imported chocolates: 12.94\nSales Tax: 8.77\nTotal: 67.98\nSum of orders: 153.81\n";
 		try {
-			Foo.main(null);
+			Foo.main(new String[] {"resources/testingSet1.xml"});
 		} catch (Exception e) {
-			fail("Foo.main threw and exception");
+			fail("Foo.main threw an exception");
 			e.printStackTrace();
 		}
-		assert(expected.compareTo(outContent.toString()) == 0);
+		assertTrue(expected.compareTo(outContent.toString()) == 0);
 	}
-
+			
 }
